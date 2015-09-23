8102927b1b38d8cc152ef1574254deeb5d4bd2c0
Brian Woodward <brian.woodward@gmail.com>
2015-09-23 16:24:34 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * emitter-only <https://github.com/doowb/emitter-only>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
